30b6fe071ba2eccec21ea72ebb6f1eb7e8878d01
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-01-18 20:26:14 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int sum=0,i,n;
+        printf("\n Please Give The Value of N:  ");
+        scanf("%d",&n);
+        for(i=1;i<=5;i++)
+        {
+            printf("\n %d",i*n);
+        }
+        
+}
