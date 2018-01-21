53b725309d4b7ab27ee7a3199a4ffd1b19372573
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-01-21 11:46:50 +0530

    Create time sub.c

---
+++ time sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int h,m,r,h1,m1,r1;
+printf("enter the hr and min");
+scanf("%d %d",&h,&m);
+printf("enter the h and m");
+scanf("%d %d",&h1,&m1);
+rr=h-h1;
+rr1=m-m1;
+printf("%d %d",r,r1);
+}
