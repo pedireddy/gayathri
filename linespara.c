792f2aaa37bbc644f97503a0623ed074ef1ade73
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-01-23 16:45:38 +0530

    Create linespara.c

---
+++ linespara.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char b[100];
+int count=0,i;
+printf("Enter the paragraph\n");
+scanf("%[^\n]s",b);
+for(i=0;b[i]!='\0';i++)
+{
+if(b[i]=='.')
+count++;
+}
+printf("Number of lines in paragraph are:%d\n",count);
+}
