1828e27d007a863445a2a47da5c7328fad8da57b
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-02-13 16:01:57 +0530

    Create 1one.c

---
+++ 1one.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+switch(n)
+{
+    case 1:printf("ONE");
+           break;
+    case 2:printf("TWO");
+           break;
+    case 3:printf("THREE");
+           break;
+    case 4:printf("FOUR");
+           break;
+    case 5:printf("FIVE");
+           break;
+    case 6:printf("SIX");
+           break;
+    case 7:printf("SEVEN");
+           break;
+    case 8:printf("EIGHT");
+           break;
+    case 9:printf("NINE");
+           break;
+    case 10:printf("TEN");
+           break;
+    default:printf("enter the limit within 1 to 10");
+}
+}
