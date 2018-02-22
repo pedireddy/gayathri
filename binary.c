3969ad8423957c5cad911af2193f8259c34875b5
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-02-22 16:11:50 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[1000];
+    scanf("%s",a);
+    if(a==0||a==1)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+
