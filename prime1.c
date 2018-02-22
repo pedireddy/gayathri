31b32f90ac34d1ff49a6932e9bdd9c9bb29ef9b6
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-02-22 16:29:42 +0530

    Create prime1.c

---
+++ prime1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+    int a, i, k= 0;
+    scanf("%d",&a);
+
+    for(i=2; i<=a/2; ++i)
+    {
+   
+        if(a%i==0)
+        {
+            k=1;
+            break;
+        }
+    }
+
+    if (k==0)
+        printf("yes");
+    else
+        printf("not");
+    
+}
+
