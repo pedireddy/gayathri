c180021c80e7a84b9f9955960b05d98e43bd418e
surumbarkulali <30828792+surumbarkulali@users.noreply.github.com>
2018-02-27 16:18:10 +0530

    Create perfsquare.c

---
+++ perfsquare.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    scanf("%d %d",&a,&b);
+    c=a*b;
+    if(a==b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("not");
+    }
+}
